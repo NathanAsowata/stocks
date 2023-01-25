7e5348c7fe205727cef6a5d8e3d1f893fc5cdbb3
Nathan Asowata <nathanasowata@gmail.com>
2023-01-25 14:38:33 +0100

    feat: added stock symbol

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
 
+interface PropTypes {
+  StockDetails:{
+  "Meta Data": {
+    "2. Symbol": string
+  }
+}}
 
-const Home = () => {
+const Home = ({StockDetails}: PropTypes) => {
   return (
-    <div>Home</div>
+    <>
+    <h2>{StockDetails["Meta Data"]["2. Symbol"]}</h2>
+
+    </>
   )
 }
 
 export default Home
+
+export async function getServerSideProps () {
+  const URL = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=30min&apikey=${process.env.API_KEY}`
+ const res = await fetch(URL)
+ const data = await res.json()
+  
+  return{
+    props: {
+      StockDetails: data
+    }
+  }
+}
