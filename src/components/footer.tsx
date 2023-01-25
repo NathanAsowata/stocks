b66f3e7b32771475201d8b862eca792b3a39d486
Nathan Asowata <nathanasowata@gmail.com>
2023-01-25 14:00:47 +0100

    style: designed the footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link"
+import styles from "../styles/footer.module.scss"
 
 const Footer = () => {
   return (
-    <footer>
-        <p>Designed and built by 
-            <Link href={"https://www.nathanasowata.com/"}>Nathan Asowata</Link>. 
-            Data provided by 
-            <Link href={"https://www.alphavantage.co/"}>Alpha Vantage API</Link>
+    <footer className={styles.footer}>
+        <p>
+            Designed and built by <Link href={"https://www.nathanasowata.com/"}>Nathan Asowata</Link>. 
+            Data provided by <Link href={"https://www.alphavantage.co/"}>Alpha Vantage API</Link>
         </p>
     </footer>
   )
