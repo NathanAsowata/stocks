ce59872241778fb76b682218af702e3caa31062f
Nathan Asowata <nathanasowata@gmail.com>
2023-01-25 13:44:53 +0100

    feat: added navigation bar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+import styles from "../styles/navbar.module.scss"
+const NavBar = () => {
+  return (
+    <nav className={styles.nav}>
+            <h1 className={styles.logo}>Stocks</h1>
+    </nav>
+  )
+}
+
+export default NavBar
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import NavBar from '@/components/navbar'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
@@ -9,6 +10,8 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Get accurate stock market information</title>
         <link rel="shortcut icon" href="icon.png" type="image/x-icon" />
       </Head>
+      <NavBar />
+      <Component {...pageProps} />
     </>
   )
 }
